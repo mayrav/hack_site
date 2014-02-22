1dd5593d35e28adf8bda63a0959eef7d1f389d30
Matthew Gallagher <mattva01@gmail.com>
2014-02-22 13:59:37 +0000

    Changed url for api

--- js/services.js
+++ js/services.js
@@ -2,20 +2,20 @@ angular.module("resourceService", ["ngResource"]).
        factory("Resource", function ($resource) {
 
            return $resource(
-               "http://hyperninja.net:4711/api/resources/:_id"
+               "http://hackapi.hyperninja.net/api/resources/:_id"
           );
       });
 
 angular.module("projectService", ["ngResource"]).
        factory("Project", function ($resource) {
            return $resource(
-               "http://hyperninja.net:4711/api/projects/:_id"   
+               "http://hackapi.hyperninja.net/api/projects/:_id"   
           );
       });
 
 angular.module("datasetService", ["ngResource"]).
        factory("DataSet", function ($resource) {
            return $resource(
-               "http://hyperninja.net:4711/api/datasets/:_id" 
+               "http://hackapi.hyperninja.net/api/datasets/:_id" 
           );
       });
