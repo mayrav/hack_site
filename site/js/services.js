b896056a6e9cec398edd47b9faa62c9947610d90
Matthew Gallagher <mattva01@gmail.com>
2014-09-10 19:24:50 -0400

    Removed dependency on personal site

--- site/js/services.js
+++ site/js/services.js
@@ -2,20 +2,20 @@ angular.module("resourceService", ["ngResource"]).
        factory("Resource", function ($resource) {
 
            return $resource(
-               "http://hackapi.hyperninja.net/api/resources/:_id"
+               "/api/resources/:_id"
           );
       });
 
 angular.module("projectService", ["ngResource"]).
        factory("Project", function ($resource) {
            return $resource(
-               "http://hackapi.hyperninja.net/api/projects/:_id"   
+               "/api/projects/:_id"   
           );
       });
 
 angular.module("datasetService", ["ngResource"]).
        factory("DataSet", function ($resource) {
            return $resource(
-               "http://hackapi.hyperninja.net/api/datasets/:_id" 
+               "/api/datasets/:_id" 
           );
       });
